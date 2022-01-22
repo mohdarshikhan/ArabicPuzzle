ebf4638d4cc1372f15c52393f4ae6211a1fdd778
Mohd Arshi Khan <arsHi@bb68>
2022-01-22 14:03:21 +0530

    Arabic, Hindi, Alphabetic and Numeric Puzzle code

---
+++ lib/src/theme_hindi.dart
@@ -0,0 +1,77 @@
+// Copyright 2020, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'core/puzzle_proxy.dart';
+import 'core/util.dart';
+import 'flutter.dart';
+import 'shared_theme.dart';
+
+const _accentBlue = Color(0xff000579);
+
+class ThemeHindi extends SharedTheme {
+  @override
+  String get name => 'Hindi';
+
+  @override
+  PuzzleType get puzzleType => PuzzleType.hindi;
+
+  const ThemeHindi();
+
+  @override
+  Color get puzzleThemeBackground => Colors.white;
+
+  @override
+  Color get puzzleBackgroundColor => Colors.white70;
+
+  @override
+  Color get puzzleAccentColor => _accentBlue;
+
+  @override
+  RoundedRectangleBorder puzzleBorder(bool small) =>
+      const RoundedRectangleBorder(
+        side: BorderSide(color: Colors.black26, width: 1),
+        borderRadius: BorderRadius.all(
+          Radius.circular(4),
+        ),
+      );
+
+  @override
+  Widget tileButton(int i, PuzzleProxy puzzle, bool small) {
+    if (i == puzzle.tileCount) {
+      assert(puzzle.solved);
+      return const Center(
+        child: Icon(
+          Icons.thumb_up,
+          size: 72,
+          color: _accentBlue,
+        ),
+      );
+    }
+
+    final correctPosition = puzzle.isCorrectPosition(i);
+
+    final content = createInk(
+      Center(
+        child: Text(
+          replaceHindiNumber((i + 1).toString()),
+          // String.fromCharCode(i + 65), // A=65
+          // (i + 1).toString(),
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: correctPosition ? FontWeight.bold : FontWeight.normal,
+            fontSize: small ? 30 : 49,
+          ),
+        ),
+      ),
+    );
+
+    return createButton(
+      puzzle,
+      small,
+      i,
+      content,
+      color: const Color.fromARGB(255, 13, 87, 155),
+    );
+  }
+}
